22f71209e13e5ed5f837a5aa9a013306097d1bb3
Ariel Noyman <noyman@mit.edu>
2019-03-27 13:18:05 -0400

    set up cityio service, fix map css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BasemapComponent } from './basemap/basemap.component';
-import { CityioComponent } from './cityio/cityio.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BasemapComponent } from "./basemap/basemap.component";
+import { CityioService } from "./cityio/cityio.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BasemapComponent,
-    CityioComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, BasemapComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private cityIOservice: CityioService) {}
+  ngOnInit() {
+    this.getCityIOalways();
+  }
+
+  getCityIOalways() {
+    setInterval(() => {
+      this.cityIOservice
+        .getCityIOdata()
+        .subscribe(cityiodata => console.log(cityiodata));
+    }, 1000);
+  }
+}
---
+++ src/app/cityio/cityio.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+
+const CITYIO_TABLE_URL = "https://cityio.media.mit.edu/api/table/";
+const DEFAULT_TABLE_NAME = "grasbrook";
+
+@Injectable({
+  providedIn: "root"
+})
+export class CityioService {
+  cityIOTableURL: string;
+  cityIOData: any;
+
+  constructor(private http: HttpClient) {
+    this.cityIOTableURL = this.getTableURL();
+  }
+
+  getTableURL(): string {
+    // return 'http://localhost:4200/assets/mock-grid-data.json';
+    console.log("using cityIO table: ", DEFAULT_TABLE_NAME);
+    return CITYIO_TABLE_URL + DEFAULT_TABLE_NAME;
+  }
+
+  getCityIOdata(): Observable<any> {
+    return this.http.get(this.cityIOTableURL).pipe(
+      tap(cityIOData => {
+        this.cityIOData = cityIOData;
+      }),
+      catchError(this.handleError("getMetadata", [])) // Still returns result (empty)
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(`${operation} failed: ${error.message}`, error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
