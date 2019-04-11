341cc812ab2b7e97040699706011491aa4f0418c
yasushi <ysshski@gmail.com>
2019-04-11 07:57:58 -0400

    basic routing: still needs work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'radar', component: RadarChartComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/radar-chart/radar-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-radar-chart',
+  templateUrl: './radar-chart.component.html',
+  styleUrls: ['./radar-chart.component.css']
+})
+export class RadarChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
