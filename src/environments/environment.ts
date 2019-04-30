d12310a0ecd7983ebbd59e6a064497777026633a
Till Degkwitz <till.degkwitz@hcu-hamburg.de>
2019-04-30 16:38:25 +0200

    Revert mapbox accessToken:

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
 
   mapbox: {
     accessToken:
-      // "pk.eyJ1IjoicmVsbm94IiwiYSI6ImNpa2VhdzN2bzAwM2t0b2x5bmZ0czF6MzgifQ.KtqxBH_3rkMaHCn_Pm3Pag"
-      "pk.eyJ1IjoiY3NpbGwiLCJhIjoiY2prY2E1eDZwMDAybzNrb3phaXgxYm5meiJ9.0NRkVQfi2GHmZFVmHoGe-g"
-      // "pk.eyJ1IjoiamVmZnJleWxlYm93c2tpOTAiLCJhIjoiY2pxdXE0MzNnMGxlMzN4bjBycmZjMWw4OCJ9.BZrqyavzNry4ejuDEcgrNQ"
+      // "pk.eyJ1IjoiY3NpbGwiLCJhIjoiY2prY2E1eDZwMDAybzNrb3phaXgxYm5meiJ9.0NRkVQfi2GHmZFVmHoGe-g"
+    // "pk.eyJ1IjoicmVsbm94IiwiYSI6ImNpa2VhdzN2bzAwM2t0b2x5bmZ0czF6MzgifQ.KtqxBH_3rkMaHCn_Pm3Pag"
+      "pk.eyJ1IjoiamVmZnJleWxlYm93c2tpOTAiLCJhIjoiY2pxdXE0MzNnMGxlMzN4bjBycmZjMWw4OCJ9.BZrqyavzNry4ejuDEcgrNQ"
   }
 };
