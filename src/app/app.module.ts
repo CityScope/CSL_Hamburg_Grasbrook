ac82daf6679b26c4818b27b4fec35fbe8a37d60f
Yasushi Sakai <ysshski@gmail.com>
2019-05-07 13:06:27 -0400

    cityio heartbeat example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, OnInit } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BasemapComponent } from "./basemap/basemap.component";
 
-import { MatIconModule, MatTooltipModule} from '@angular/material';
+import { MatIconModule, MatTooltipModule } from '@angular/material';
 
 import { HomeComponent } from "./home/home.component";
 import { RadarChartComponent } from "./radar-chart/radar-chart.component";
 import { MapSettingComponent } from './basemap/map-controls/map-setting/map-setting.component';
-import { LegendComponent} from "./basemap/map-controls/legend/legend.component";
-import { LayerControlComponent} from "./basemap/map-controls/layer-control/layer-control.component";
-import {ConfigurationService} from "./service/configuration.service";
-import {GridLayerService} from "./service/grid-layer.service";
+import { LegendComponent } from "./basemap/map-controls/legend/legend.component";
+import { LayerControlComponent } from "./basemap/map-controls/layer-control/layer-control.component";
+import { ConfigurationService } from "./service/configuration.service";
+import { GridLayerService } from "./service/grid-layer.service";
 
 @NgModule({
   declarations: [
---
+++ src/app/service/cityio.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { interval } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CityioService {
+
+  baseUrl = "https://cityio.media.mit.edu/api/table/";
+  tableName = "virtual_table";
+  url = `${this.baseUrl}${this.tableName}`;
+
+  table_data: any = null; // can be accessed by other components, this will always be updated
+
+  update = interval(1000); // this is just rsjs way of a interval
+
+  constructor(private http: HttpClient) { 
+    this.update.subscribe((n)=>{
+      this.http.get(this.url).subscribe((data)=> this.table_data = data);
+    }) 
+  }
+}
