354bcab43c474f6f027dc986a3aae82f1b05d7dc
Ariel Noyman <noyman@mit.edu>
2019-05-08 18:09:49 -0400

    Home page menu for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { HomeComponent } from './home/home.component';
-import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { HomeComponent } from "./home/home.component";
+import { RadarChartComponent } from "./radar-chart/radar-chart.component";
+import { BasemapComponent } from "./basemap/basemap.component";
 
 const routes: Routes = [
-  { path:'', component: HomeComponent },
-  { path:'radar', component: RadarChartComponent } 
+  { path: "", component: HomeComponent },
+  { path: "radar", component: RadarChartComponent },
+  { path: "map", component: BasemapComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
