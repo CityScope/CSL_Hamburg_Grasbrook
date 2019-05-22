6189864c3b589b1bfe74f6c99aeca72e5c78170f
Ariel Noyman <noyman@mit.edu>
2019-05-22 15:38:03 -0400

    removed grid component and test radar cityio interval

--- src/app/service/layer-loader.service.ts
+++ src/app/service/layer-loader.service.ts
@@ -1,21 +1,15 @@
 import { Injectable } from "@angular/core";
 import { CsLayer } from "../../typings";
-import { GridLayer } from '../layer/grid.layer';
 import { ConfigurationService } from "./configuration.service";
 
 @Injectable({
   providedIn: "root"
 })
-export class LayerLoaderService  {
-
-  constructor(
-    private gridMaker: GridLayer,
-    private config: ConfigurationService) {}
+export class LayerLoaderService {
+  constructor(private config: ConfigurationService) {}
 
   getLayers(): CsLayer[] {
     const layers: CsLayer[] = [];
-    const cityioLayer = this.gridMaker.makeGridFromCityIO();
-    layers.push(cityioLayer);
     Array.prototype.push.apply(layers, this.config.layers);
 
     return layers;
