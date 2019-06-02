292d40fe62a8fb46e466f4694fbc6ceea8d4d854
Ariel Noyman <noyman@mit.edu>
2019-06-02 04:50:02 -0400

    replaced mapbox token with url restiction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,6 @@ export const environment = {
 
   mapbox: {
     accessToken:
-      // "pk.eyJ1IjoiY3NpbGwiLCJhIjoiY2prY2E1eDZwMDAybzNrb3phaXgxYm5meiJ9.0NRkVQfi2GHmZFVmHoGe-g"
-    // "pk.eyJ1IjoicmVsbm94IiwiYSI6ImNpa2VhdzN2bzAwM2t0b2x5bmZ0czF6MzgifQ.KtqxBH_3rkMaHCn_Pm3Pag"
-      "pk.eyJ1IjoiamVmZnJleWxlYm93c2tpOTAiLCJhIjoiY2pxdXE0MzNnMGxlMzN4bjBycmZjMWw4OCJ9.BZrqyavzNry4ejuDEcgrNQ"
+      "pk.eyJ1IjoicmVsbm94IiwiYSI6ImNqd2VwOTNtYjExaHkzeXBzYm1xc3E3dzQifQ.X8r8nj4-baZXSsFgctQMsg"
   }
 };
