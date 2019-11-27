c9677a4ba4e841c185b3a8e411f13add287b3409
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2019-11-27 18:26:18 +0100

    update - home UI by login state

--- cityscope_grasbrook_angular/src/app/login/login.component.ts
+++ cityscope_grasbrook_angular/src/app/login/login.component.ts
@@ -54,7 +54,6 @@ export class LoginComponent implements OnInit {
       .pipe(first())
       .subscribe(
         data => {
-          console.log(data);
           this.router.navigate([this.returnUrl]);
         },
         error => {
