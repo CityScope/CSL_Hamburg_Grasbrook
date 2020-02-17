4c3e4ff488229f36cd4d8578de3d43b5a77be867
Jonas <jonas.luft@hcu-hamburg.de>
2020-02-17 14:40:28 +0100

    fixed empty window on /map without being logged in

--- src/app/utils/auth.guard.ts
+++ src/app/utils/auth.guard.ts
@@ -18,7 +18,7 @@ export class AuthGuard implements CanActivate {
       return true;
     }
 
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+    this.router.navigate(['/']);
     return false;
   }
 }
